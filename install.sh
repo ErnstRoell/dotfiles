12241399c73945d5cf900e39d4cc79d985e9760e
Ernst Roell <ernstroell@gmail.com>
2025-05-26 00:20:44 +0200

    install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+# Install base
+sudo apt update 
+sudo apt upgrade
+sudo apt install zsh
+chsh -s $(which zsh)
+
+# Install nvim 
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+# Github cli 
+
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
+git config --global user.email "ernstroell@gmail.com"
+git config --global user.name "Ernst Roell"
+
+
